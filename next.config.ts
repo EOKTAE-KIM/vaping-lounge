1c6ffc64d5f9af502e989690d8e51671ede60e30
EOKTAE-KIM <eoktae1002@gmail.com>
2026-03-30 12:02:14 +0900

    Cloudflare wrangler 및 OpenNext 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.*", "192.168.0.50", "203.234.214.137"],
 };
+
+initOpenNextCloudflareForDev();
 
 export default nextConfig;
